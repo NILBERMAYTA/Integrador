ba0af1a55bbbb6d81634c9ea30ddfebd7f1c2825
Nilber <nilberrmayta@gmail.com>
2025-10-07 00:18:21 -0400

    Creacion de archivos y uso de estilos de Penguin UI

---
+++ app/Models/OperacionDetalle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OperacionDetalle extends Model
+{
+    /** @use HasFactory<\Database\Factories\OperacionDetalleFactory> */
+    use HasFactory;
+}
---
+++ app/Models/TipoIncidente.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TipoIncidente extends Model
+{
+    /** @use HasFactory<\Database\Factories\TipoIncidenteFactory> */
+    use HasFactory;
+}
